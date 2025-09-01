cd7155b268b70ded625d44813b1a4d1ae671f823
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-09-02 00:39:28 +0600

    Action Button Dynamically added

--- frontend/src/app/dashboard/users/page.tsx
+++ frontend/src/app/dashboard/users/page.tsx
@@ -168,7 +168,7 @@ const UserManagement = () => {
               ]}
               exportFileName="users_list"
               paginationRowsPerPageOptions={[10, 20, 50, 100]}
-              defaultPerPage={2}
+              defaultPerPage={10}
               searchPlaceholder="Search users..."
             />
 
