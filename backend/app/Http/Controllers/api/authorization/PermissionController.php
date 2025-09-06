4f2e3eed56890cc53fb50d32529475c8e231ebef
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-09-06 16:47:42 +0600

    Permission module started

--- backend/app/Http/Controllers/api/authorization/PermissionController.php
+++ backend/app/Http/Controllers/api/authorization/PermissionController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\api\authorization;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class PermissonController extends Controller
+class PermissionController extends Controller
 {
     //
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\auth\AuthController;
 use App\Http\Controllers\api\UserController;
 use App\Http\Controllers\api\authorization\RoleController;
+use App\Http\Controllers\api\authorization\PermissionController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
@@ -21,6 +22,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/roles', [RoleController::class, 'store']);
     Route::put('/roles/{role}', [RoleController::class, 'update']);
     Route::delete('/roles/{role}', [RoleController::class, 'destroy']);
+
+    //Roles manage routes
+    Route::get('/permissions', [PermissionController::class, 'index']);
+    Route::post('/permissions', [PermissionController::class, 'store']);
+    Route::put('/permissions/{permission}', [PermissionController::class, 'update']);
+    Route::delete('/permissions/{permission}', [PermissionController::class, 'destroy']);
 });
 
 Route::get('/test-cors', function () {
