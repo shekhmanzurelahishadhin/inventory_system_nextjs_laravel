86802ee81f3d19d94164aa7e5e0e0928cfec30bb
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-09-09 23:09:43 +0600

    Backend permission check

--- backend/app/Http/Controllers/api/authorization/RoleController.php
+++ backend/app/Http/Controllers/api/authorization/RoleController.php
@@ -8,30 +8,40 @@ use App\Http\Requests\authorization\roles\UpdateRoleRequest;
 use App\Http\Resources\authorization\RoleResource;
 use App\Services\RoleService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('permission:role.create|role.view|role.edit|role.delete')->only('index');
-        $this->middleware('permission:role.create')->only('store');
-        $this->middleware('permission:role.edit')->only('update');
-        $this->middleware('permission:role.delete')->only('destroy');
-    }
+//    public function __construct()
+//    {
+//        $this->middleware('permission:role.create|role.view|role.edit|role.delete')->only('index');
+//        $this->middleware('permission:role.create')->only('store');
+//        $this->middleware('permission:role.edit')->only('update');
+//        $this->middleware('permission:role.delete')->only('destroy');
+//    }
 
     public function index(Request $request, RoleService $roleService)
     {
+        $user = auth()->user();
+        dd(
+            $user->getAllPermissions(), // see what permissions it detects
+            $user->roles,               // check role guard
+            $user->hasPermissionTo('role.view'),        // should be false now
+            $user->hasPermissionTo('role.view', 'sanctum')  // should be true
+        );
+
         $perPage = $request->get('per_page', 10);
         $filters = $request->only('search');
 
         $roles = $roleService->getRoles($filters, $perPage);
-
+        $hasPermission = auth()->user()->getAllPermissions();
         return response()->json([
             'data' => RoleResource::collection($roles),
             'total' => $roles->total(),
             'current_page' => $roles->currentPage(),
             'per_page' => $roles->perPage(),
+            'perm' => $hasPermission,
         ]);
     }
 
