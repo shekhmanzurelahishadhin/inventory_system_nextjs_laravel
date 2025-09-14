1efca6fe9a713b8ebd8a2208726a3ef9fd662b20
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-09-14 22:10:51 +0600

    loader button loader added

--- frontend/src/app/dashboard/authorization/roles/page.tsx
+++ frontend/src/app/dashboard/authorization/roles/page.tsx
@@ -71,9 +71,9 @@ const Roles = () => {
     },
   ];
 
-  // useEffect(() => {
-  //   setIsMounted(true);
-  // }, []);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
   const openModal = (type: "create" | "edit" | "view", role: any = null) => {
     setModalType(type);
@@ -236,7 +236,7 @@ const Roles = () => {
     },
   ];
 
-  // if (!isMounted) return <Preloader />;
+  if (!isMounted) return <Preloader />;
 
   return (
     <>
@@ -324,6 +324,12 @@ const Roles = () => {
                       : "opacity-100"
                   }`}
                 >
+                   {isSubmitting ?
+                    <svg className="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                      <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                    </svg> : ''
+                  }
                   {isSubmitting
                     ? modalType === "create"
                       ? "Creating..."
