6956cf252c4b63b68cd93538757106981ee8293f
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-10-03 19:04:17 +0600

    Sub Category Model Created

--- backend/app/Models/softConfig/Category.php
+++ backend/app/Models/softConfig/Category.php
@@ -12,4 +12,20 @@ class Category extends Model
     use HasFactory, SoftDeletes, SetSlugAndAuditing;
 
     protected $guarded = ['id'];
+
+
+    public function subCategories()
+    {
+        return $this->hasMany(SubCategory::class);
+    }
+
+    protected static function booted()
+    {
+        static::deleting(function ($category) {
+            if (!$category->isForceDeleting()) {
+                // Soft delete all sub categories
+                $category->subCategories()->delete();
+            }
+        });
+    }
 }
---
+++ backend/app/Models/softConfig/SubCategory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\softConfig;
+
+use App\Traits\SetSlugAndAuditing;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use function Carbon\this;
+
+class SubCategory extends Model
+{
+    use HasFactory, SoftDeletes, SetSlugAndAuditing;
+
+    protected $guarded = ['id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
