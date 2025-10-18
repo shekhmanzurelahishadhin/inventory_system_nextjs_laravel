b1b3d5125d6797511b7da70017e6dba88a93c2ee
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-10-18 11:06:47 +0600

    Migration File updated

--- backend/database/migrations/2025_10_18_105333_create_suppliers_table.php
+++ backend/database/migrations/2025_10_18_105333_create_suppliers_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('suppliers', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->string('name');
             $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
             $table->string('phone')->nullable();
