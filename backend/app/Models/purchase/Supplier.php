17c7aa0c58d44b3401f002b2ed75c687da96ec25
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-10-18 11:01:40 +0600

    Migration File Added

---
+++ backend/app/Models/purchase/Supplier.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\purchase;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Supplier extends Model
+{
+    //
+}
---
+++ backend/database/migrations/2025_10_18_105333_create_suppliers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->id();
+            $table->string('name');
+            $table->foreignId('company_id')->constrained('companies')->onDelete('cascade');
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->text('address')->nullable();
+            $table->decimal('opening_balance', 15, 2)->default(0)->comment('Opening balance'); // Opening balance
+            $table->enum('opening_balance_type', ['DR', 'CR'])->default('DR')->comment('Debit / Credit'); // Debit / Credit
+            $table->boolean('status')->default(true);
+            $table->softDeletes();
+            $table->timestamps();
+
+            // Auditing columns
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suppliers');
+    }
+};
