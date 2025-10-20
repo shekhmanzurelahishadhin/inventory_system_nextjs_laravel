aa7a6964f4664b9993dc33dd2086cd17f76405ef
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-10-20 17:43:46 +0600

    Resource Added For supplier

--- backend/app/Http/Resources/purchase/SupplierResource.php
+++ backend/app/Http/Resources/purchase/SupplierResource.php
@@ -14,6 +14,22 @@ class SupplierResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'phone' => $this->phone,
+            'email' => $this->email,
+            'company_id' => $this->company?->id ?? null,
+            'company_name' => $this->company?->name ?? null,
+            'slug' => $this->slug,
+            'status' => $this->status,
+            'address' => $this->address,
+            'created_by' => $this->createdBy?->name,
+            'updated_by' => $this->updated_by,
+            'deleted_by' => $this->deleted_by,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'deleted_at' => $this->deleted_at,
+        ];
     }
 }
